caa3e8f6e2d7b96ea5828fda1e5c0d1f869bcba9
老周部落 <laozhoubuluo@126.com>
2021-07-07 13:56:29 +0800

    修复 语言包不匹配

--- laozhoubuluo_ranklistlimit.class.php
+++ laozhoubuluo_ranklistlimit.class.php
@@ -20,10 +20,10 @@ class plugin_laozhoubuluo_ranklistlimit {
 			$unitlowerlimit = is_numeric($_G['cache']['plugin']['laozhoubuluo_ranklistlimit']['unitlowerlimit']) ? intval($_G['cache']['plugin']['laozhoubuluo_ranklistlimit']['unitlowerlimit']) : 0;
 			$unithigherlimit = is_numeric($_G['cache']['plugin']['laozhoubuluo_ranklistlimit']['unithigherlimit']) ? intval($_G['cache']['plugin']['laozhoubuluo_ranklistlimit']['unithigherlimit']) : 2147483647;
 			if($unitprice < $unitlowerlimit) {
-				showmessage(lang('plugin/laozhoubuluo_ranklistlimit', 'lower_unit', array('now' => $unitprice, 'limit' => $unitlowerlimit)));
+				showmessage(lang('plugin/laozhoubuluo_ranklistlimit', 'lower_unit', array('num' => $unitprice, 'limit' => $unitlowerlimit)));
 			}
 			if($unitprice > $unithigherlimit) {
-				showmessage(lang('plugin/laozhoubuluo_ranklistlimit', 'higher_unit', array('now' => $unitprice, 'limit' => $unithigherlimit)));
+				showmessage(lang('plugin/laozhoubuluo_ranklistlimit', 'higher_unit', array('num' => $unitprice, 'limit' => $unithigherlimit)));
 			}
 		}
 
@@ -34,10 +34,10 @@ class plugin_laozhoubuluo_ranklistlimit {
 			$creditlowerlimit = is_numeric($_G['cache']['plugin']['laozhoubuluo_ranklistlimit']['creditlowerlimit']) ? intval($_G['cache']['plugin']['laozhoubuluo_ranklistlimit']['creditlowerlimit']) : 0;
 			$credithigherlimit = is_numeric($_G['cache']['plugin']['laozhoubuluo_ranklistlimit']['credithigherlimit']) ? intval($_G['cache']['plugin']['laozhoubuluo_ranklistlimit']['credithigherlimit']) : 2147483647;
 			if($showcredit < $creditlowerlimit) {
-				showmessage(lang('plugin/laozhoubuluo_ranklistlimit', 'lower_credit_friend', array('now' => $showcredit, 'limit' => $creditlowerlimit)));
+				showmessage(lang('plugin/laozhoubuluo_ranklistlimit', 'lower_credit_friend', array('num' => $showcredit, 'limit' => $creditlowerlimit)));
 			}
 			if($showcredit > $credithigherlimit) {
-				showmessage(lang('plugin/laozhoubuluo_ranklistlimit', 'higher_credit_friend', array('now' => $showcredit, 'limit' => $credithigherlimit)));
+				showmessage(lang('plugin/laozhoubuluo_ranklistlimit', 'higher_credit_friend', array('num' => $showcredit, 'limit' => $credithigherlimit)));
 			}
 		}
 
@@ -50,16 +50,16 @@ class plugin_laozhoubuluo_ranklistlimit {
 			$unitlowerlimit = is_numeric($_G['cache']['plugin']['laozhoubuluo_ranklistlimit']['unitlowerlimit']) ? intval($_G['cache']['plugin']['laozhoubuluo_ranklistlimit']['unitlowerlimit']) : 0;
 			$unithigherlimit = is_numeric($_G['cache']['plugin']['laozhoubuluo_ranklistlimit']['unithigherlimit']) ? intval($_G['cache']['plugin']['laozhoubuluo_ranklistlimit']['unithigherlimit']) : 2147483647;
 			if($unitprice < $unitlowerlimit) {
-				showmessage(lang('plugin/laozhoubuluo_ranklistlimit', 'lower_unit', array('now' => $unitprice, 'limit' => $unitlowerlimit)));
+				showmessage(lang('plugin/laozhoubuluo_ranklistlimit', 'lower_unit', array('num' => $unitprice, 'limit' => $unitlowerlimit)));
 			}
 			if($unitprice > $unithigherlimit) {
-				showmessage(lang('plugin/laozhoubuluo_ranklistlimit', 'higher_unit', array('now' => $unitprice, 'limit' => $unithigherlimit)));
+				showmessage(lang('plugin/laozhoubuluo_ranklistlimit', 'higher_unit', array('num' => $unitprice, 'limit' => $unithigherlimit)));
 			}
 			if($showcredit < $creditlowerlimit) {
-				showmessage(lang('plugin/laozhoubuluo_ranklistlimit', 'lower_credit', array('now' => $showcredit, 'limit' => $creditlowerlimit)));
+				showmessage(lang('plugin/laozhoubuluo_ranklistlimit', 'lower_credit', array('num' => $showcredit, 'limit' => $creditlowerlimit)));
 			}
 			if($showcredit > $credithigherlimit) {
-				showmessage(lang('plugin/laozhoubuluo_ranklistlimit', 'higher_credit', array('now' => $showcredit, 'limit' => $credithigherlimit)));
+				showmessage(lang('plugin/laozhoubuluo_ranklistlimit', 'higher_credit', array('num' => $showcredit, 'limit' => $credithigherlimit)));
 			}
 		}
 
